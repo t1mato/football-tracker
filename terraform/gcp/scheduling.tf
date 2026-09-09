bc980d1956f59cb4f2cc51be0a5344f973038336
t1mato <112602997+t1mato@users.noreply.github.com>
2026-09-09 16:33:35 -0700

    feat: Cloud Scheduler nightly trigger for the pipeline workflow

--- terraform/gcp/scheduling.tf
+++ terraform/gcp/scheduling.tf
@@ -46,3 +46,29 @@ resource "google_workflows_workflow" "nightly_pipeline" {
 
   depends_on = [google_project_service.workflows]
 }
+
+resource "google_cloud_scheduler_job" "nightly_trigger" {
+  project     = var.project_id
+  name        = "nightly-trigger"
+  region      = var.region
+  description = "Triggers the nightly ingest+transform Cloud Workflow."
+  schedule    = "0 2 * * *"
+  time_zone   = "UTC"
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://workflowexecutions.googleapis.com/v1/projects/${var.project_id}/locations/${var.region}/workflows/${google_workflows_workflow.nightly_pipeline.name}/executions"
+    headers = {
+      "Content-Type" = "application/json"
+    }
+    body = base64encode(jsonencode({
+      argument = jsonencode({})
+    }))
+
+    oidc_token {
+      service_account_email = google_service_account.pipeline_orchestrator.email
+    }
+  }
+
+  depends_on = [google_project_service.cloud_scheduler]
+}
